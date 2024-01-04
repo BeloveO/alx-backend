ad29a0fd7f06cb137cc76f46b970961b2082189c
Belove <beloveolusola@gmail.com>
2024-01-04 03:02:41 +0100

    Last in first out

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python3
 """
-FIFO caching module
+Last In First Out caching module
 """
 
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """
-    Caching system
+    Last in First out Caching system
     """
     def __init__(self):
         """
         initialize
         """
         super().__init__()
-        self.order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """
         assign to the dict self.cache_data the item value for key
         """
         if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length > BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[0]))
-                del self.cache_data[self.order[0]]
-                del self.order[0]
-            self.order.append(key)
-            self.cache_data[key] = item
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """
