55c731b9aa48aa8504b78316b5d592d21f09542a
Belove <beloveolusola@gmail.com>
2024-01-04 02:38:45 +0100

    FIFO

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+FIFO
+"""
+
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """
+    Caching system
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        assign to the dict self.cache_data the item value for key
+        """
+        if key is None or item is None:
+            return
+        
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+        return self.cache_data.get(key, None)
