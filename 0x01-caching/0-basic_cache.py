a8c25c0244c04a6e1deef0358646714e1657b926
Belove <beloveolusola@gmail.com>
2024-01-04 02:27:28 +0100

    Cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+BaseCaching module
+"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Caching system
+    """
+    def put(self, key, item):
+        """
+        assign to the dict self.cache_data the item value for key
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+        return self.cache_data.get(key, None)
