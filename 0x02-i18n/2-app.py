943a095c0c0f720e86f8b8f4082a1be7b6869c17
Belove <beloveolusola@gmail.com>
2024-01-10 00:37:51 +0100

    Basic flask-babel app

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Flask Babel instantiation
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Babel Class configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def index():
+    """
+    Index page routing
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
