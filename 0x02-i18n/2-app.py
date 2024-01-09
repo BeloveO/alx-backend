f6ac9f37ada7df47bcb5865b6de91a2e76f98233
Belove <beloveolusola@gmail.com>
2024-01-10 00:45:47 +0100

    Basic flask-babel locale lang

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,13 +19,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """
-    Getting the suited language for a locale
+    Retrieves suited locale
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
-
 
 
 @app.route("/")
